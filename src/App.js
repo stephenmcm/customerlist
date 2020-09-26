be1e256bcd2c647b102cb5f9150e6901ac0a3fcd
Stephen McMahon <stephentmcm@gmail.com>
2020-09-26 14:07:13 +1000

    Rename App. Remove boilerplate display

--- src/App.js
+++ src/App.js
@@ -6,51 +6,14 @@ import "./App.scss";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
+      <header>
+        <h1>Customer List</h1>
       </header>
+      <footer>
+        Made by Stephen McMahon{" "}
+        <a href="mailto:stephentmcm@gmail.com">stephentmcm@gmail.com</a>{" "}
+        <a href="https://github.com/stephenmcm">@stephenmcm</a>
+      </footer>
     </div>
   );
 }
