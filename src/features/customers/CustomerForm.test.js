6a29efa37e9b7e0b541fe8308b78cb90107bec44
Stephen McMahon <stephentmcm@gmail.com>
2020-09-26 17:03:42 +1000

    Add form test for empty values

--- src/features/customers/CustomerForm.test.js
+++ src/features/customers/CustomerForm.test.js
@@ -50,3 +50,40 @@ it("Handles correct values", async () => {
   const dateOfBirthErrElement = queryByText(/Birthday is required/i);
   expect(dateOfBirthErrElement).toBeNull();
 });
+
+it("Handles empty values", async () => {
+  const { container, queryByText } = render(<StoreMockedCustomerForm />);
+  const firstName = container.querySelector('input[name="firstName"]');
+  const lastName = container.querySelector('input[name="lastName"]');
+  const dateOfBirth = container.querySelector('input[name="dateOfBirth"]');
+
+  userEvent.clear(firstName);
+  await waitFor(() => {
+    fireEvent.blur(firstName);
+  });
+  expect(firstName).toHaveAttribute("value", "");
+  const firstNameErrElement = queryByText(/First name is required/i);
+  expect(firstNameErrElement).toBeInTheDocument();
+
+  userEvent.type(lastName, "");
+  await waitFor(() => {
+    fireEvent.blur(lastName);
+  });
+  const lastNameErrElement = queryByText(/Last name is required/i);
+  expect(lastNameErrElement).toBeInTheDocument();
+
+  // @TODO Research why the userEvent doesn't work here. It's marked fixed:
+  // https://github.com/testing-library/user-event/pull/400
+  // First revelvant google result:
+  // https://github.com/testing-library/user-event/issues/399
+  await waitFor(() => {
+    fireEvent.change(dateOfBirth, {
+      target: {
+        value: "bad",
+      },
+    });
+    fireEvent.blur(dateOfBirth);
+  });
+  const dateOfBirthErrElement = queryByText(/Birthday is required/i);
+  expect(dateOfBirthErrElement).toBeInTheDocument();
+});
