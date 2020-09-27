1883645516ce258be88d9ffaeabb74a6da87fd0d
Stephen McMahon <stephentmcm@gmail.com>
2020-09-27 16:33:19 +1000

    Fix temp initial custoemr id setting

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,57 @@
 import { configureStore } from "@reduxjs/toolkit";
-import customersReducer from "../features/customers/customersSlice";
+import customersReducer, {
+  nextcustomer,
+} from "../features/customers/customersSlice";
 import visibilityFilterReducer from "../features/filters/filtersSlice";
+
+const intialDummyState = {
+  customers: {
+    lastID: 3,
+    byID: {
+      0: {
+        id: 0,
+        firstName: "Stephen",
+        lastName: "McMahon",
+        dateOfBirth: "1987-06-29",
+        deleted: false,
+        searchText: "stephen mcmahon",
+      },
+      1: {
+        id: 1,
+        firstName: "Ada",
+        lastName: "Lovelace",
+        dateOfBirth: "1815-12-10",
+        deleted: false,
+        searchText: "ada lovelace",
+      },
+      2: {
+        id: 2,
+        firstName: "Alan",
+        lastName: "Turing",
+        dateOfBirth: "1912-06-23",
+        deleted: false,
+        searchText: "alan turing",
+      },
+      3: {
+        id: 3,
+        firstName: "Tim",
+        lastName: "Berners-Lee",
+        dateOfBirth: "2020-09-09",
+        deleted: false,
+        searchText: "tim berners-lee",
+      },
+    },
+  },
+};
+
+// @TODO swap to UUIDs or otherwise handle this better as when it gets out of
+// sync with the list we risk overwritting data.
+nextcustomer.Id = 4;
 
 export default configureStore({
   reducer: {
     customers: customersReducer,
     visibilityFilter: visibilityFilterReducer,
   },
+  preloadedState: intialDummyState,
 });
--- src/features/customers/customersSlice.js
+++ src/features/customers/customersSlice.js
@@ -1,45 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-// @TODO swapp to UUIDs or otherwise handle this better as when it gets out of
+// @TODO swap to UUIDs or otherwise handle this better as when it gets out of
 // sync with the list we risk overwritting data
-let nextcustomerId = 0;
-
-const intialDummyState = {
-  byID: {
-    0: {
-      id: 0,
-      firstName: "Stephen",
-      lastName: "McMahon",
-      dateOfBirth: "1987-06-29",
-      deleted: false,
-      searchText: "stephen mcmahon",
-    },
-    1: {
-      id: 1,
-      firstName: "Ada",
-      lastName: "Lovelace",
-      dateOfBirth: "1815-12-10",
-      deleted: false,
-      searchText: "ada lovelace",
-    },
-    2: {
-      id: 2,
-      firstName: "Alan",
-      lastName: "Turing",
-      dateOfBirth: "1912-06-23",
-      deleted: false,
-      searchText: "alan turing",
-    },
-    3: {
-      id: 3,
-      firstName: "Tim",
-      lastName: "Berners-Lee",
-      dateOfBirth: "2020-09-09",
-      deleted: false,
-      searchText: "tim berners-lee",
-    },
-  },
-};
+// Tested swapping to nanoid and ran into "TypeError: (0 , _nanoid.nanoid) is not a function"
+// in tests. Github issue here: https://github.com/facebook/create-react-app/pull/8768
+export const nextcustomer = { Id: 0 };
 
 const customersSlice = createSlice({
   name: "customers",
@@ -63,7 +28,7 @@ const customersSlice = createSlice({
             firstName,
             lastName,
             dateOfBirth,
-            id: nextcustomerId++,
+            id: nextcustomer.Id++,
           },
         };
       },
